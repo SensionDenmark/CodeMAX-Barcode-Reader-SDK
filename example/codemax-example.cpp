4bba13bfad4ffa9cdd803a746573a3a83c2baf9d
Andreas Aakerberg <aakerberg@me.com>
2018-07-18 13:30:05 +0200

    updated example

--- example/codemax-example.cpp
+++ example/codemax-example.cpp
@@ -37,50 +37,174 @@
 ///------------------------------------------------------------------------------------------------------------
 ///------------------------------------------------------------------------------------------------------------
 ///
-
-
-// First we import the CodeMAX Barcode Reader SDK library 
-// and instantiate objects and variables:
- 
 #include "codemax.h"
 #include "cmdefines.h"
- 
+
+
 using namespace cm;
- 
+const static enumString boolToYesNo[] = {L"No", L"Yes"};
+
+
+///---------------------------------------------------------------------------------------------------------
+///---------------------------------------------------------------------------------------------------------
+///
+/// \brief This demo program shows how to use the CodeMAX SDK in simple steps :
+///
+/// 1)  The goal is to create a small command-line program which takes two arguments, namely arg1: filename and
+///     path of the source image to be used for analysis, and arg2: the filename and path of the output image
+///     that will contain information about the decoding.
+///
+/// 2)  Retrieve CodeMAX SDK software version info and print it on the console.
+///
+/// 3)  Open the specified input image
+///
+/// 4)  Perform setup of the CodeMAX barcode decoder instance by calling setup() once. In this example the decoder
+///     is configured to:
+///
+///     -Use precise processing strategy i.e. 'CM_PROCSTRATEGY_PRECISE'
+///     -Use a processing timeout
+///     -Enable decoding of five different symbol types, namely: EAN8, EAN13, CODE128, QR and Datamatrix symbols
+///
+/// 5) Define a processing area 'processingArea' that is equal to the full image, and call the decode function decodeImage().
+///
+/// 6) Display the decoding result on the console.
+///
+/// 7) Save the output image at the specified location and with the specified name.
+///
+///---------------------------------------------------------------------------------------------------------
+///---------------------------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
- CodeMAX decoder;
- cmIoImg inputImage;
- cmIoImg resultImage;
- cmDecodeInfo decodeInfo;
- int numSymbols;
- 
- // Then we load the test image:
- inputImage.load("image.jpg");
- 
- // Next we initialize the decoder to decode barcodes using 
- // the precise mode allow maximum 100 ms to decode a barcode
- // before time-out and enable all supported symbologies.
- decoder.setup(enumProcStrategy::CM_PROCSTRATEGY_PRECISE, 100, CM_ENABLE_ALL);
- 
- // Now we are ready to start decoding, which is done by providing
- // the input image and an optional empty image which will contain 
- // information about the decoded barcodes:
- numSymbols = decoder.decodeImage(inputImage, NULL, &resultImage);
- 
- // Finally, we save the result image to the disk, and iterate over
- // all decoded barcodes and print relevant information about the 
- // barcodes to the terminal.
- resultImage.save("resultImage.jpg");
- 
- std::cout << "CodeMAX Barcode Reader SDK C++ Example\n" << "\n";
- 
- for (int i = 0; i < numSymbols; i++)
- {
-  decoder.getBarcodeRegionInfo(i, decodeInfo);
-  std::cout << "Barcode type: << decodeInfo.barcodeType << "\n";
-  std::cout << "Barcode content: " << decodeInfo.barcodeContent << "\n";
- }
- 
- return 0;
+    CodeMAX             decoder;
+    cmString            errorMessage;
+    cmIoImg             testImage;
+    cmIoImg             resultImage;
+    cmRoi               processingArea;
+    cmDecodeInfo        decodeInfo;
+    cmString            className;
+    cmString            outputFilename;
+    cmString            infoString;
+    cmVersionInfo       versionInfo;
+    enumCmErrorCodes    status;
+
+
+    /// 1 Check the argument list
+    if (argc != 3)
+    {
+        std::cout << "------------------------------------------------------------------------------------------------------\n\n";
+        std::cout << "         S E N S I O N    C O D E M A X    B A R C O D E    D E C O D E R    SDK    D E M O\n";
+        std::cout << "\nThis CodeMAX SDK demo application decodes all barcodes in an input image, shows the result on the\n";
+        std::cout << "console output and stores a result image with decode information\n";
+        std::cout << "Call the application with the following arguments:\n\n";
+        std::cout << "\n";
+        std::cout << "./codemax_demo <inputFilename> <outputFilename>\n\n";
+        std::cout << "\n";
+        std::cout << "<inputFilename>\t The path to the input image.\n";
+        std::cout << "<outputFilename>\t The path to the result image.\n";
+        std::cout << "------------------------------------------------------------------------------------------------------\n\n";
+
+        return 1;
+    }
+
+    /// 2 Retrieve software version information and display it in a header
+    decoder.getVersionInfo(versionInfo);
+    infoString.format(L"  Software: %ls\n  Version : %d.%d.%d\n  Release : %s\n",
+                      enumLicenseTypeName[(int)versionInfo.licenseType], versionInfo.versionMajor, versionInfo.versionMinor,
+                      versionInfo.revisionNumber, versionInfo.versionReleaseDate);
+
+    // Some info header
+    std::cout << "------------------------------------------------------------------------------------------------------\n\n";
+    std::cout << "         S E N S I O N    C O D E M A X    B A R C O D E    D E C O D E R    SDK    D E M O\n\n";
+    std::cout << infoString.getStringA() << "\n";
+    std::cout << "----Arguments-----------------------------------------------------------------------------------------\n\n";
+    std::cout << "  inputFilename : " << argv[1] << "\n";
+    std::cout << "  outputFilename: " << argv[2] << "\n";
+    std::cout << "----Result--------------------------------------------------------------------------------------------\n\n";
+
+
+    /// 3 Open an image from the disc
+    if (testImage.load(argv[1]) != IO_ERROR_NONE)
+    {
+        std::cout << "  Error: Input image load error" "\n";
+        return 1;
+    }
+
+    /// 4 Initialize the decoder. Here we only want to decode 5 different types of symbols
+    /// and with a timeout value of 500 ms. We use the precise processing strategy
+    ///
+    status = decoder.setup(CM_PROCSTRATEGY_PRECISE, 500, CM_EAN_8 | CM_EAN_13 | CM_CODE_128 | CM_DATAMATRIX | CM_QR);
+
+    if (status != CM_ERROR_NONE)
+    {
+        // Print the error message
+        errorMessage = decoder.getErrorMessage(status);
+        std::cout << "  ERROR: " << errorMessage.getStringA() << "\n";
+        std::cout << "------------------------------------------------------------------------------------------------------\n\n";
+        return 1;
+    }
+
+
+    /// 5 Create a user-defined processing area that covers the whole image (alternatively the procROI and pResult arguments could
+    /// also have been set to NULL), and finally decode the image
+    processingArea.left     = 0;
+    processingArea.right    = testImage.getWidth();
+    processingArea.top      = 0;
+    processingArea.bottom   = testImage.getHeight();
+
+    status = decoder.decodeImage(testImage, decodeInfo, &processingArea, &resultImage);
+    if (status != CM_ERROR_NONE)
+    {
+        // Print the error message
+        std::cout << "  ERROR: The decoder failed to execute due to the following error:\n";
+        errorMessage = decoder.getErrorMessage(status);
+        std::cout << "         " << errorMessage.getStringA() << "\n";
+        std::cout << "------------------------------------------------------------------------------------------------------\n\n";
+        return 1;
+    }
+
+
+    /// 6 Retrieve the decoding result and print it
+    ///
+    std::cout << "Number of found symbols:" << decodeInfo.numBarcodes << "\n";
+
+    for (int i = 0; i < decodeInfo.numBarcodes; i++)
+    {
+
+        // Print the result
+        std::cout << "Symbol " << i << ":" << std::endl;
+        className.format(L"%ls", enumSymbolClassName[(int)decodeInfo.barcodeData[i].detectedClass]);
+        std::cout << "  Detected class....:" << (char *)className.getStringA() << std::endl;
+        std::cout << "  Decoded...........:" << (decodeInfo.barcodeData[i].isDecoded ? "yes" : "no")  << std::endl;
+        std::cout << "  Timed out.........:" << (decodeInfo.barcodeData[i].hasTimedOut ? "yes" : "no")  << std::endl;
+        std::cout << "  Symbol content....:" << decodeInfo.barcodeData[i].barcodeContent  << std::endl;
+        std::cout << "  Symbol type.......:" << decodeInfo.barcodeData[i].barcodeType  << std::endl;
+        std::cout << "  Symbol proc.time..:" << decodeInfo.barcodeData[i].decodingTimeMs  << " ms" << std::endl;
+        std::cout << "  Symbol region.....:" << "left:" << decodeInfo.barcodeData[i].barcodeRoi.left << " top:" <<
+                  decodeInfo.barcodeData[i].barcodeRoi.top << " right:" << decodeInfo.barcodeData[i].barcodeRoi.right << " bottom:" <<
+                  decodeInfo.barcodeData[i].barcodeRoi.bottom  << std::endl;
+        std::cout << "\n" << std::endl;
+
+    }
+
+    std::cout << "------------------------------------------------------------------------------------------------------\n\n";
+
+
+    /// 7 Save the result image with overlay information
+    outputFilename.copy(argv[2]);
+    if (resultImage.save(outputFilename, &infoString) != IO_ERROR_NONE)
+    {
+        std::cout << "  Error: Output image save error. " << infoString.getStringA() << std::endl;
+        std::cout << "------------------------------------------------------------------------------------------------------\n\n";
+        return 1;
+    }
+
+    // Return 0 if barcodes were found, 1 otherwise.
+    if (decodeInfo.numBarcodes)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
